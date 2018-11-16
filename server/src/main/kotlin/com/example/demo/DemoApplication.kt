4236a9af5083d11aa297b61c344ee8fe721a019b
rengel <robin.engel@qudosoft.de>
2018-11-16 20:40:48 +0100

    first commit

---
+++ server/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -0,0 +1,64 @@
+package com.example.demo
+
+import kotlinx.html.*
+import kotlinx.html.stream.appendHTML
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType.TEXT_HTML
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.BodyInserters.fromObject
+import org.springframework.web.reactive.function.server.router
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
+
+@Configuration
+class RouterConfig {
+
+    @Bean
+    fun route(helloWorldHandler: HelloWorldHandler) = router {
+        "/hello".nest {
+            GET("") {
+                ok()
+                        .contentType(TEXT_HTML)
+                        .body(fromObject(buildString {
+                            appendHTML().body {
+                                helloWorldHandler.handle(this)
+                            }
+                        }))
+            }
+        }
+        "/**" {
+            notFound().build()
+        }
+    }
+
+}
+
+@Component
+class HelloWorldHandler {
+    fun handle(body: BODY) {
+        body.run {
+            custom {
+                +"Hallo Welt!"
+            }
+
+            p {
+                +"Some text for testing"
+            }
+        }
+    }
+}
+
+fun HtmlBlockTag.custom(block: () -> Unit = {}) = h1 {
+    span {
+        +"Custom: "
+    }
+    block()
+}
